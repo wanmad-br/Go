c5da38fdc5d8101b3beacace069131340f659d09
Wander Madeira <wanmad-br@github.com>
2025-12-17 23:21:50 +0000

    code sample

---
+++ SimpleSum/simplesum.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// add is a function that takes two integers and returns their sum
+func add(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	// Declare variables
+	var name string = "Student"
+	num1 := 10 // Short variable declaration
+	num2 := 5
+
+	// Call the function
+	result := add(num1, num2)
+
+	// Print results to the console
+	fmt.Printf("Hello, %s!\n", name)
+	fmt.Printf("The sum of %d and %d is: %d\n", num1, num2, result)
+}
